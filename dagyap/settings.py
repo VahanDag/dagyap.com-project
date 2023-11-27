c53fa52ba30095d07760a1781244a4eee4832b4b
Vahan Dag <80410212+VahanDag@users.noreply.github.com>
2023-11-28 02:45:38 +0300

    Update settings.py

--- dagyap/settings.py
+++ dagyap/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=n-g#56^jbpy2blc&fi&5ai1vq#1gmgr#58urk8_xut90z-!x*'
+SECRET_KEY = 'DELETED'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
